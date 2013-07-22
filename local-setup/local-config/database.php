bcbb9e583fc126d9e841c4f5b07ec8e21cb3ff9d
Brian Jacobel <bjacobel@theatlantic.com>
2013-07-22 11:35:26 -0400

    One branch to rule them all.
    The local/master approach to git is WAY too complex. from here on out, three files
    will separate the local and master branch: .htaccess, app/config/config.php, and
    app/config/database.php. They will be gitignored, so you can make changes to them locally
    and they will NEVER affect production settings unless you add -f (which would be very dumb).
    
    Now you can develop locally on the same branch as prod runs on and not worry about burning
    everything to the ground on accident. In theory.
    
    Files needed to set up your computer to develop locally are now in dir local-setup.
    Two ini files (php, ini) are drop-in replacements for your system-wide PHP and Apache configs.
    Three files in local-config are drop-in replacements for files in this repository
    
    They go in the following places (run commands from bonus root)
    cp local-setup/local-config/.htaccess .htaccess
    cp local-setup/local-config/config.php application/config/config.php
    cp local-setup/local-config/database.php application/config/database.php
    
    All three will require some configuration to match your local hostname and MySQL
    settings. It should be pretty self-explanatory. YMMV. @bjacobel

--- local-setup/local-config/database.php
+++ local-setup/local-config/database.php
@@ -45,24 +45,24 @@
 | the active record class
 */
 
-$active_group = 'medved';
+$active_group = 'default';
 $active_record = TRUE;
 
-$db['medved']['hostname'] = 'localhost';
-$db['medved']['username'] = 'orientdba';
-$db['medved']['password'] = 'bgtyhn768594';
-$db['medved']['database'] = 'DB02Orient';
-$db['medved']['dbdriver'] = 'mysql';
-$db['medved']['dbprefix'] = '';
-$db['medved']['pconnect'] = TRUE;
-$db['medved']['db_debug'] = TRUE;
-$db['medved']['cache_on'] = FALSE;
-$db['medved']['cachedir'] = '';
-$db['medved']['char_set'] = 'utf8';
-$db['medved']['dbcollat'] = 'utf8_general_ci';
-$db['medved']['swap_pre'] = '';
-$db['medved']['autoinit'] = TRUE;
-$db['medved']['stricton'] = FALSE;
+$db['default']['hostname'] = 'localhost';
+$db['default']['username'] = 'orientdba';
+$db['default']['password'] = '0r1en!';
+$db['default']['database'] = 'DB02Orient';
+$db['default']['dbdriver'] = 'mysql';
+$db['default']['dbprefix'] = '';
+$db['default']['pconnect'] = TRUE;
+$db['default']['db_debug'] = TRUE;
+$db['default']['cache_on'] = FALSE;
+$db['default']['cachedir'] = '';
+$db['default']['char_set'] = 'utf8';
+$db['default']['dbcollat'] = 'utf8_general_ci';
+$db['default']['swap_pre'] = '';
+$db['default']['autoinit'] = TRUE;
+$db['default']['stricton'] = FALSE;
 
 /* End of file database.php */
 /* Location: ./application/config/database.php */
